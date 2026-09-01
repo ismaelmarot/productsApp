3d0aa22a23717a5587a8f02b197b202fa286c4a7
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-09-01 09:00:49 -0300

    refact: CategoryView.tsx

--- frontend/src/views/CategoryViews/CategoryViews.tsx
+++ frontend/src/views/CategoryViews/CategoryViews.tsx
@@ -1,32 +1,32 @@
-import type { CategoryViewProps } from '../../interfaces/CategoryView.interface';
-import AddCategory from '../../components/Categories/AddCategory/AddCategory';
-import DetailsCategory from '../../components/Categories/DetailsCategory/DetailsCategory';
-import EditCategory from '../../components/Categories/EditCategory/EditCategory';
-import ListCategories from '../../components/Categories/ListCategories/ListCategories';
-import DeleteCategory from '../../components/Categories/DeleteCategory/DeleteCategory';
+import type { CategoryViewProps } from '../../interfaces/CategoryView.interface'
+import AddCategory from '../../components/Categories/AddCategory/AddCategory'
+import DetailsCategory from '../../components/Categories/DetailsCategory/DetailsCategory'
+import EditCategory from '../../components/Categories/EditCategory/EditCategory'
+import ListCategories from '../../components/Categories/ListCategories/ListCategories'
+import DeleteCategory from '../../components/Categories/DeleteCategory/DeleteCategory'
 
 function CategoryViews({ view, setView, selectedCategory, setSelectedCategory, onDone }: CategoryViewProps) {
   switch (view) {
     case 'addCategory':
-      return <AddCategory onCategoryAdded={(c) => setSelectedCategory(c)} />;
+      return <AddCategory onCategoryAdded={(c) => setSelectedCategory(c)} />
     case 'detailsCategory':
-      return <DetailsCategory />;
+      return <DetailsCategory />
     case 'editCategory':
-      return <EditCategory onUpdated={onDone} />;
+      return <EditCategory onUpdated={onDone} />
     case 'listCategory':
-      return <ListCategories />;
+      return <ListCategories />
     case 'deleteCategory':
       return (
         <DeleteCategory
              onCategoryDeleted={(name) => {
-            alert(`Categoría eliminada: ${name}`);
+            alert(`Categoría eliminada: ${name}`)
             onDone();
           }}
         />
       );
     default:
-      return null;
+      return null
   }
 }
 
-export default CategoryViews;
+export default CategoryViews
